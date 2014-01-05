6a77ed0e94bc4cad8f3418319806c8ccd8bcba2e
awwa <awwa500@gmail.com>
2014-01-05 16:13:40 +0900

    change env

--- config/environments.rb
+++ config/environments.rb
@@ -1,8 +1,7 @@
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
-    db = URI.parse(ENV['HEROKU_POSTGRESQL_BROWN_URL'] ||
-	     URI.parse(ENV['DATABASE_URL'] || 
+    db = URI.parse(ENV['DATABASE_URL'] || 
         'postgres://localhost/mydb')
  
 	ActiveRecord::Base.establish_connection(
